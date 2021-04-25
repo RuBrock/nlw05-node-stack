69e5c6a9e840752eee2dec386d8454225c8e60ff
Rubens Brock <r.brocksilva@yahoo.com.br>
2021-04-25 19:29:43 -0300

    finished client admin interations, project done

--- src/websocket/admin.ts
+++ src/websocket/admin.ts
@@ -7,7 +7,7 @@ io.on("connect", async (socket) => {
   const messagesService = new MessagesService();
 
   const allConnectionsWithoutAdmin = await connectionsService.findAllWithoutAdmin();
-
+  
   io.emit("admin_list_all_users", allConnectionsWithoutAdmin);
 
   socket.on("admin_list_messages_by_user", async (params, callback) => {
@@ -16,5 +16,31 @@ io.on("connect", async (socket) => {
     const allMessages = await messagesService.listByUser(user_id);
 
     callback(allMessages);
+  });
+
+  socket.on("admin_send_message", async (params) => {
+    const { text, user_id } = params;
+
+    await messagesService.create({
+      text,
+      user_id,
+      admin_id: socket.id
+    });
+
+    const { socket_id } = await connectionsService.findByUserId(user_id);
+    
+    io.to(socket_id).emit("admin_send_to_client", {
+      text,
+      socket_id: socket.id
+    });
+
+  });
+
+  socket.on("admin_user_in_support", async (params) => {
+    const { user_id } = params;
+    await connectionsService.updateAdminID(user_id, socket.id);
+    
+    io.emit("admin_list_all_users", allConnectionsWithoutAdmin);
   })
-})
+
+});
